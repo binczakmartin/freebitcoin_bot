264b0bca76267f19ed063fd160e6ded7e565f6c1
Martin Binczak <bincza_m@etna-alternance.net>
2020-09-13 22:37:31 +0200

    test

--- index.js
+++ index.js
@@ -675,10 +675,10 @@ async function processAvailableAccounts() {
             nb_roll = 0;
             utils.log(1, "processAvailableAccounts()", "try to roll "+accounts.length+" accounts");
             while(accounts.length) {
-                console.log("test");
                 chunk = accounts.splice(0, nb_acc);
                 for (elem of chunk) {
                     // var proxyUrl = proxies[i].protocol+"://"+proxies[i].ip+":"+proxies[i].port;
+                    console.log("test");
                     var testProxy = await checkProxy(elem.proxy);
                     if (testProxy == 1) {
                         var current_email = elem.email; // bug bizarre
